484d0a8d82170e69b7ed03dc26e197d5ab114e65
Romayle <romayle.dharmasena@gmail.com>
2024-05-23 11:30:21 +0530

    backend commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const workoutRoutes = require('./routes/workouts')
+// const cors = require('cors')
+
+
+//create express app
+const app = express()
+
+// app.use(cors({origin:"input the localhost in your frontend"}))
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+// routes
+app.use('/api/workouts', workoutRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
